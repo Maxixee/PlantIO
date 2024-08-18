c6ac63eed87632fe4e4403d3c853ba0337d6741f
Hiego <hiegobarreto@gmail.com>
2024-08-18 19:09:34 -0300

    Segunda tela

--- lib/main.dart
+++ lib/main.dart
@@ -8,49 +8,159 @@ class PlantIOApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFFAB8654), // Cor de fundo marrom
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'PlantIO',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstScreen(),
+        '/plantInfo': (context) => PlantInfoScreen(),
+      },
+    );
+  }
+}
+
+// Tela inicial
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFAB8654), // Cor de fundo marrom
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'PlantIO',
+              style: TextStyle(
+                fontFamily: 'Roboto', // Substitua pelo nome correto da fonte usada
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 20),
+            Image.asset(
+              'assets/plant_image.png', // Substitua pelo caminho da sua imagem
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/plantInfo'); // Navega para a segunda tela
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black, // Cor do botão
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              ),
+              child: Text(
+                'Começar',
                 style: TextStyle(
-                  fontFamily: 'Roboto', // Substitua pelo nome correto da fonte usada
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                  color: Colors.white,
+                  fontSize: 18,
                 ),
               ),
-              SizedBox(height: 20),
-              Image.asset(
-                'assets/plant_image.png', // Substitua pelo caminho da sua imagem
-                width: 200,
-                height: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Tela informação planta
+class PlantInfoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFAB8654), // Cor de fundo marrom
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/strawberry_plant.png', // Substitua pelo caminho correto da sua imagem
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(10),
               ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {
-                  // Ação ao clicar no botão
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black, // Cor do botão
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Pé de morango',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                ),
-                child: Text(
-                  'Começar',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
+                  SizedBox(height: 10),
+                  Text(
+                    'Consumir morangos regularmente pode melhorar a saúde do coração, regular o açúcar no sangue, e fornecer fibras essenciais para a digestão.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação para adicionar saber
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  ),
+                  child: Text(
+                    'Adicionar saber',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação para consultar saberes
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  ),
+                  child: Text(
+                    'Consultar saberes',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
